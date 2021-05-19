708d8cca5ce16f83f08ab1d391486bc0b8100462
ooyama ryohei <bm1205@outlook.jp>
2021-05-19 08:15:17 +0000

    rails5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'tags/new'
   get 'users/new'
   get 'pages/index'
@@ -6,5 +7,8 @@ Rails.application.routes.draw do
   get 'pages/help'
   resources :users
   get 'pages/link'
+   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
