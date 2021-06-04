56f7de515206d286015a703e568d6937a56e1da3
ooyama ryohei <bm1205@outlook.jp>
2021-06-04 10:00:49 +0000

    rails 確認テスト5

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,7 +11,7 @@ class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.new(comment_params)
     
-    if @comment.save
+    if @comment.save!
       redirect_to topics_path, success: 'コメント登録に成功しました'
     else
       flash.now[:danger] = "コメント登録に失敗しました"
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   has_many :topics
+ 
   validates :user_id, presence: true
   validates :text, presence: true
 end
